610293a8d21ad7c7658bae6027386627a2fcbfaf
Maximilian Kaden <mkadenfilgo@gmail.com>
2025-01-12 00:51:20 +0100

    wp

--- src-tauri/src/application/useCases/connect_to_xplane.rs
+++ src-tauri/src/application/useCases/connect_to_xplane.rs
@@ -26,6 +26,15 @@ impl ConnectToXPlaneUseCase {
             xplane_rest::fetch_datarefs()
         );
 
+        let datarefs = datarefs_result.map_err(|e| e.to_string())?;
+
+        let battery_dataref = datarefs
+            .iter() // Itera sobre &DataRef
+            .find(|d| d.name == "AirbusFBW/BatOHPArray") // Verifica o campo name
+            .ok_or("DataRef da bateria não encontrado")?;
+
+        println!("DataRef da bateria encontrado: ID {}", battery_dataref.id);
+
         let (tx, rx) = oneshot::channel();
 
         let battery1_clone = self.battery_1.clone();
@@ -44,7 +53,6 @@ impl ConnectToXPlaneUseCase {
         self.battery_1.set_state(BatteryState::Auto);
 
         let ws_state = ws_result.map_err(|e| e.to_string())?;
-        let datarefs = datarefs_result.map_err(|e| e.to_string())?;
 
         println!("Lista de DataRefs disponíveis:");
         // for dataref in &datarefs {
